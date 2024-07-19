3e1dbe15687a253659cc954fc8b4c5bbe9e1f10c
Charles Fauchet <charles.fauchet@gmail.com>
2024-07-19 10:48:34 +0800

    Hide Django secret key

--- htfbi_backend/htfbi_backend/settings.py
+++ htfbi_backend/htfbi_backend/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-s0!(c9jr0s_n$73yqk&wcvnb=x3)s^=m*d+pk2e1cxqjz7p*2%'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
