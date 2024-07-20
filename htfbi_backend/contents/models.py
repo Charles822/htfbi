cc4b9b07f40e0cd6c6bb00a3c87501f35d569751
Charles Fauchet <charles.fauchet@gmail.com>
2024-07-20 10:38:43 +0800

    Create Video and transcript models

--- htfbi_backend/contents/models.py
+++ htfbi_backend/contents/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+class Video(models.Model):
+    youtube_url = models.URLField(max_length=255)
+    video_id = models.CharField(max_length=50)
+    title = models.CharField(max_length=255, blank=True, null=True)
+    channel_name = models.CharField(max_length=255)
+    published_at = models.DateTimeField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title or self.video_id
+
+
+class Transcript(models.Model):
+    transcript_text = models.TextField(null=True, blank=True)
+    video = models.ForeignKey(
+        Video, on_delete=models.CASCADE, related_name='transcripts')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'Transcript for {self.video.title or self.video.video_id}'
