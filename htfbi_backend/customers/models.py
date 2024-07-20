f525db4bcd1fce72cc36b76a3a5bafc54f0bfc5e
Charles Fauchet <charles.fauchet@gmail.com>
2024-07-20 10:11:49 +0800

    Create Customer model

--- htfbi_backend/customers/models.py
+++ htfbi_backend/customers/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+class Customer(models.Model):
+    MEMBERSHIP_FREE = 'F'
+    MEMBERSHIP_CONTRIBUTOR = 'C'
+
+    MEMBERSHIP_CHOICES = [
+        (MEMBERSHIP_FREE, 'Free'),
+        (MEMBERSHIP_CONTRIBUTOR, 'Contributor'),
+    ]
+    birth_date = models.DateField(null=True, blank=True)
+    membership = models.CharField(
+        max_length=1, choices=MEMBERSHIP_CHOICES, default=MEMBERSHIP_FREE)
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.user.first_name} {self.user.last_name}'
