35f6e67f284092fd3cdaab0809aed9b3fefe8ef9
Charles Fauchet <charles.fauchet@gmail.com>
2024-07-27 09:25:51 +0800

    Created Note model

--- htfbi_backend/notes/models.py
+++ htfbi_backend/notes/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Note(models.Model):
+    video = models.ForeignKey('contents.Video', on_delete=models.CASCADE)
+    response = models.OneToOneField('ai_agent.Response', on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Note for Video ID {self.video.video_id}"
+
+    class Meta:
+        app_label = 'notes'
