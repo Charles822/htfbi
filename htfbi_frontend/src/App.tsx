54a651882bfb01a1b7e332fd6c3fc4503c5c972d
Charles Fauchet <charles.fauchet@gmail.com>
2024-08-06 14:47:08 +0800

    Setting up shadcn and test blocks

--- htfbi_frontend/src/App.tsx
+++ htfbi_frontend/src/App.tsx
@@ -1,15 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import { Button } from "@/components/ui/button"
+import { Button } from './components/ui/button';
+import Login from './Pages/Login';
+import Dashboard from './Pages/Dashboard';
 
 function App() {
-
   return (
-    <>
-
-    </>
+    <div>
+      <Dashboard />
+    </div>
   )
 }
 
