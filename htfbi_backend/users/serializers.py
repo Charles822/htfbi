9166dd40a9f32ce25c139607c5d9eb51a216822b
Charles Fauchet <charles.fauchet@gmail.com>
2024-08-29 10:53:16 +0800

    Created user creation endpoint

--- htfbi_backend/users/serializers.py
+++ htfbi_backend/users/serializers.py
@@ -2,12 +2,35 @@ from django.contrib.auth.models import User
 from rest_framework import serializers
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
-from .models import Profile
+from .models import Profile, Customer
+
+
+class UserCreationSerializer(serializers.ModelSerializer):
+    email = serializers.EmailField(required=True)
+    username = serializers.CharField(required=True)
+    password = serializers.CharField(write_only=True, required=True)
+
+    def create(self, validated_data):        
+
+        new_user = User.objects.create_user(
+        email=validated_data['email'],
+        username=validated_data['username'],
+        password=validated_data['password'],
+        )
+        
+        return new_user   
+
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = '__all__'
+
+class CustomerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Customer
+        fields = '__all__'
+
 
 class ProfileSerializer(serializers.ModelSerializer):
     user = UserSerializer(many=False, read_only=True)
--- htfbi_backend/users/views.py
+++ htfbi_backend/users/views.py
@@ -7,13 +7,42 @@ from rest_framework_simplejwt.views import TokenObtainPairView
 from django.contrib.auth.models import User
 from django.conf import settings
 from .models import Customer, Profile
-from .serializers import UserSerializer, ProfileSerializer, MyTokenObtainPairSerializer
+from .serializers import UserSerializer, ProfileSerializer, CustomerSerializer, MyTokenObtainPairSerializer
 from core.permissions import AdminOnly
+
+
+def get_permissions_based_on_action(action):
+    # No permission required to create a user
+    if action == 'create':
+        return [AllowAny]
+    # For other actions, only allow the owner or an admin
+    else:
+        return [IsOwnerOrAdmin]
+
 
 class UserViewSet(ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
-    permission_classes = [AdminOnly]
+
+    def get_permissions(self):
+        return [permission() for permission in get_permissions_based_on_action(self.action)]
+
+    @action(detail=False, methods=['post'], url_path='create_user')
+    def add_agent(self, request):
+        serializer = UserCreationSerializer(data=request.data)
+        
+        if serializer.is_valid():
+            new_user = serializer.save()
+            response_serializer = UserSerializer(new_user)
+            return Response(response_serializer.data, status=status.HTTP_201_CREATED)
+        
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class CustomerViewSet(ModelViewSet):
+    queryset = Customer.objects.all()
+    serializer_class = CustomerSerializer
+    permission_classes = [IsAuthenticated]
 
 
 class ProfileViewSet(ModelViewSet):
